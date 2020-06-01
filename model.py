7a55eff35f174eb769aef8f554b7d946dd6736a2
dosp0911 <dosp0911@gmail.com>
2020-06-01 21:54:05 +0900

    u_net++ model

--- model.py
+++ model.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
 
 
-import torch 
+import torch
 import torch.nn as nn
 from torchsummary import summary
 
-from collections import OrderedDict
 import math
+
 
 class Con2D(nn.Module):
     def __init__(self, in_c, out_c, k_size, padding=1, is_bn=True):
@@ -36,68 +32,82 @@ class Con2D(nn.Module):
         return self.sequential(x)
 
 
-def crop(features, size):
-    h_old, w_old = features[0][0].size()
-    h, w = size
-    x = math.ceil((h_old - h) / 2)
-    y = math.ceil((w_old - w) / 2)
-    return features[:, :, x:(x + h), y:(y + w)]
-
-class U_net(nn.Module):
+class U_net_pp(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(U_net, self).__init__()
+        super(U_net_pp, self).__init__()
 
-        self.con_block_1 = Con2D(in_channels, 64, 3)
-        self.con_block_2 = Con2D(64, 128, 3)
-        self.con_block_3 = Con2D(128, 256, 3)
-        self.con_block_4 = Con2D(256, 512, 3)
-        self.con_block_5 = Con2D(512, 1024, 3)
+        filters = [32, 64, 128, 256, 512]
+        filter_size = 3
+        self.up = nn.UpsamplingBilinear2d(scale_factor=2)
 
-        self.exp_block_4 = Con2D(1024, 512, 3, is_bn=False)
-        self.exp_block_3 = Con2D(512, 256, 3, is_bn=False)
-        self.exp_block_2 = Con2D(256, 128, 3, is_bn=False)
-        self.exp_block_1 = Con2D(128, 64, 3, is_bn=False)
+        self.pool = nn.MaxPool2d(2, 2)
 
-        self.deconv_4 = nn.ConvTranspose2d(1024, 512, 2, stride=2)
-        self.deconv_3 = nn.ConvTranspose2d(512, 256, 2, stride=2)
-        self.deconv_2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
-        self.deconv_1 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.conv0_0 = Con2D(in_channles, filters[0], filter_size)
+        self.conv1_0 = Con2D(filters[0], filters[1], filter_size)
+        self.conv2_0 = Con2D(filters[1], filters[2], filter_size)
+        self.conv3_0 = Con2D(filters[2], filters[3], filter_size)
+        # self.conv4_0 = Con2D(filters[3], filters[4], filter_size)
 
-        self.final_layer = nn.Conv2d(64, out_channels, 1)
+        self.conv0_1 = Con2D(filters[0]+filters[1], filters[0], filter_size)
+        self.conv1_1 = Con2D(filters[1]+filters[2], filters[1], filter_size)
+        self.conv2_1 = Con2D(filters[2]+filters[3], filters[2], filter_size)
+        # self.conv3_1 = Con2D(filters[3]+filters[4], filters[3], filter_size)
+
+        self.conv0_2 = Con2D(filters[0]*2+filters[1], filters[0], filter_size)
+        self.conv1_2 = Con2D(filters[1]*2+filters[2], filters[1], filter_size)
+        # self.conv2_2 = Con2D(filters[2]*2+filters[3], filters[2], filter_size)
+
+        self.conv0_3 = Con2D(filters[0]*3+filters[1], filters[0], filter_size)
+        # self.conv1_3 = Con2D(filters[1]*3+filters[2], filters[1], filter_size)
+
+        # self.conv0_4 = Con2D(filters[0]*4+filters[1], filters[0], filter_size)
+
+        self.final = Con2D(filters[0], out_channels, 1, padding=0)
+        self.init_weights()
 
     def forward(self, x):
-        con_block_1_out = self.con_block_1(x)
-        x = nn.MaxPool2d(2, stride=2)(con_block_1_out)
-        con_block_2_out = self.con_block_2(x)
-        x = nn.MaxPool2d(2, stride=2)(con_block_2_out)
-        con_block_3_out = self.con_block_3(x)
-        x = nn.MaxPool2d(2, stride=2)(con_block_3_out)
-        con_block_4_out = self.con_block_4(x)
-        x = nn.MaxPool2d(2, stride=2)(con_block_4_out)
-        x = self.con_block_5(x)
+        x0_0 = self.conv0_0(x)
+        x1_0 = self.conv1_0(self.pool(x0_0))
+        x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], dim=1))
 
-        x = self.deconv_4(x)
-        x = torch.cat([con_block_4_out, x], dim=1)
-        x = self.exp_block_4(x)
+        x2_0 = self.conv2_0(self.pool(x1_0))
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0)], dim=1))
+        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], dim=1))
 
-        x = self.deconv_3(x)
-        x = torch.cat([con_block_3_out, x], dim=1)
-        x = self.exp_block_3(x)
+        x3_0 = self.conv3_0(self.pool(x2_0))
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0)], dim=1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1)], dim=1))
+        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], dim=1))
 
-        x = self.deconv_2(x)
-        x = torch.cat([con_block_2_out, x], dim=1)
-        x = self.exp_block_2(x)
+        # x4_0 = self.conv4_0(self.pool(x3_0))
+        # x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], dim=1))
+        # x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1)], dim=1))
+        # x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2)], dim=1))
+        # x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], dim=1))
 
-        x = self.deconv_1(x)
-        x = torch.cat([con_block_1_out, x], dim=1)
-        x = self.exp_block_1(x)
+        return self.final(x0_3)
 
-        x = self.final_layer(x)
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, std=1e-3)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
 
-        return x
 
-
-if __name__ =="__main__":
+if __name__ == '__main__':
     from torchsummary import summary
-    u_net = U_net(1, 4)
-    summary(u_net, (1, 256, 1600), device='cpu')
+    u_net_pp = U_net_pp(12, 4)
+    s = summary(u_net_pp, (12, 40, 40), device='cpu')
+    print(s)
--- pre_train_model.py
+++
@@ -1,8 +0,0 @@
-import torchvision.models as models
-
-pre_vgg = models.vgg16_bn(pretrained=True)
-
-for name, param in pre_vgg.named_parameters():
-    print(name, param)
-
-pre_vgg
